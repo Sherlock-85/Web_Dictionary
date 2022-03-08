7b99fca8349349fa76056cef8a2e775961f7af60
Sherlock-85 <cholmes1111@gmail.com>
2022-03-08 15:29:44 -0500

    Create navigation menu for Homepage

--- webapp/home.py
+++ webapp/home.py
@@ -2,11 +2,31 @@ import justpy as jp
 
 class Home:
     path = "/"
+    @classmethod
+    def serve(cls, req):
+        wp = jp.QuasarPage(tailwind=True)
 
-    def serve(self):
-        wp = jp.QuasarPage(tailwind=True)
-        wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_model="left",
+                            bordered=True)
+        scroller = jp.QScrollArea(a=drawer, classes="fit")
+        qlist = jp.QList(a=scroller)
+        a_classes = "p-2 m-2 text-lg text-blue-400 hover:text-blue-700"
+        jp.A(a=qlist, text="Home", href="/", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="Dictionary", href="/dictionary", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="About", href="/about", classes=a_classes)
+        jp.Br(a=qlist)
+
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu",
+                click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
+        container = jp.QPageContainer(a=layout)
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="This is the Home page!", classes="text-4xl m-2")
         jp.Div(a=div, text=""""
         Programming isn't about what you know; it's about what you can figure out.” - Chris Pine
@@ -21,6 +41,13 @@ class Home:
                      - Joyce Wheeler
         """, classes="text-lg")
         return wp
+
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
 
 
 
